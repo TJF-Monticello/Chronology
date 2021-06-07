a87493b4e161f95b71fd8132cef8f513aa19a1ee
pete <pete.alonzi@gmail.com>
2021-06-07 09:24:30 -0400

    Create Requirements.R

---
+++ Requirements.R
@@ -0,0 +1,13 @@
+# This file contains the R packages that need to be installed to run the codee
+
+# load the libraries
+install.packages('RPostgreSQL')
+install.packages('dplyr')
+install.packages('tidyr')
+install.packages('reshape2')
+install.packages('ca')
+install.packages('plotrix')
+install.packages('ggplot2')
+install.packages('viridis')
+install.packages('ggrepel')
+
